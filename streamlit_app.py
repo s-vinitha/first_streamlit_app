65271595d7e57f756ddb03d9978f5f50c152cd6e
s-vinitha <149043713+s-vinitha@users.noreply.github.com>
2023-11-09 21:17:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ except URLError as e:
 
 
 
-streamlit.stop()
+
 
 # import snowflake.connector
 
@@ -71,4 +71,4 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
-
+streamlit.stop()
